c4508347ea323269e6487086f04ec203358a3c69
Bruno Ferreira <ferreira.sousa.bruno@gmail.com>
2018-07-06 16:57:18 +0100

    Add notifications, add presentation

---
+++ admin-server/src/main/java/io/mercedesbenz/springio/adminserver/ApplicationConfiguration.java
@@ -0,0 +1,46 @@
+package io.mercedesbenz.springio.adminserver;
+
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.notify.CompositeNotifier;
+import de.codecentric.boot.admin.server.notify.Notifier;
+import de.codecentric.boot.admin.server.notify.RemindingNotifier;
+import de.codecentric.boot.admin.server.notify.filter.FilteringNotifier;
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    private final InstanceRepository repository;
+    private final ObjectProvider<List<Notifier>> otherNotifiers;
+
+    public ApplicationConfiguration(InstanceRepository repository, ObjectProvider<List<Notifier>> otherNotifiers) {
+        this.repository = repository;
+        this.otherNotifiers = otherNotifiers;
+    }
+
+    @Bean
+    public FilteringNotifier filteringNotifier() {
+        CompositeNotifier delegate = new CompositeNotifier(otherNotifiers.getIfAvailable(Collections::emptyList));
+        return new FilteringNotifier(delegate, repository);
+    }
+
+    @Primary
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public RemindingNotifier remindingNotifier() {
+        RemindingNotifier notifier = new RemindingNotifier(filteringNotifier(), repository);
+        notifier.setReminderPeriod(Duration.ofMillis(5000));
+        notifier.setCheckReminderInverval(Duration.ofSeconds(10));
+        return notifier;
+    }
+    
+}
