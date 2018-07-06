61d9c12307172cf146da856c663c1ac224a29c64
Bruno Ferreira <ferreira.sousa.bruno@gmail.com>
2018-07-06 17:06:58 +0100

    remove auditable

--- admin-client-autodb/src/main/java/io/mercedesbenz/springio/adminclientautodb/Vehicle.java
+++ admin-client-autodb/src/main/java/io/mercedesbenz/springio/adminclientautodb/Vehicle.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Vehicle extends MyAuditable<String> implements Serializable{
+public class Vehicle implements Serializable{
 
     private static final long serialVersionUID = 8226459989560781366L;
 
